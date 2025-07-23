de8c86d1ddc80dba69eba6bc272357b9e5c2b84b
Akrifis <kadzzuta@gmail.com>
2025-07-24 00:57:45 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,17 +1,4 @@
 // Инициализация Firebase
-const firebaseConfig = {
-    apiKey: "AIzaSyBn5pWh0pmWXJ1qSEbG_c00EIubr86a-TU",
-    authDomain: "licoforice-98586.firebaseapp.com",
-    projectId: "licoforice-98586",
-    storageBucket: "licoforice-98586.firebasestorage.app",
-    messagingSenderId: "478236357284",
-    appId: "1:478236357284:web:3e7d736b335bc8bd082c31"
-};
-
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
-const auth = firebase.auth();
-
 // Загрузка аниме
 function loadAnime() {
     db.collection("anime").get().then((querySnapshot) => {
