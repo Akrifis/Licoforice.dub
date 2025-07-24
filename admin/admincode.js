1b4c0979a7a908947a51fa0faeb1060134f09ddd
Akrifis <kadzzuta@gmail.com>
2025-07-24 11:13:07 +0600

    Delete admin directory

--- admin/admincode.js
+++
@@ -1,6 +0,0 @@
-// В начале admin.html
-const password = prompt('daz1io0:');
-if (password !== 'daz1io0') {
-  document.body.innerHTML = '<h1>Доступ запрещен</h1>';
-  throw new Error('Unauthorized');
-}
