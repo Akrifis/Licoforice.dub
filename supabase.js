56eb54800f1ad9febcf2a85199f67ce928b94f83
Akrifis <kadzzuta@gmail.com>
2025-07-24 16:52:27 +0600

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
-const supabaseUrl = 'https://your-project-id.supabase.co'
-const supabaseKey = 'your-public-key'
+const supabaseUrl = 'https://xqpesqrjwwhfwoguejxm.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhxcGVzcXJqd3doZndvZ3VlanhtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMzNDU1MTQsImV4cCI6MjA2ODkyMTUxNH0.z7e2cXNhIO7CHDeeuaNIUXnpsWmwjyHILD8Y1i0sW0U'
 export const supabase = createClient(supabaseUrl, supabaseKey)
